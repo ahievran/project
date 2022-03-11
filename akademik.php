22063a4ee6f246b8088d147eba12ac2f6c8ebe5b
kaynarburak <62210378+kaynarburak@users.noreply.github.com>
2022-03-11 15:26:18 +0300

    video mesajı eklendi

--- akademik.php
+++ akademik.php
@@ -1,49 +1,102 @@
 <?php include 'header.php'; ?>
-    <main class="page blog-post-list">
-        <section class="clean-block clean-blog-list dark">
-            <div class="container-fluid">
-                <div class="block-heading" style="margin-bottom: 53px;">
-                    <h2 class="text-info">Akademik Kadromuz</h2>
-                </div>
-                <div class="block-content">
-                    <div class="clean-blog-post">
-                        <div class="row" style="box-shadow: 0px 0px 7px var(--bs-gray-400);">
-                            <div class="col-md-auto col-lg-auto col-xl-auto"><img class="img-fluid" src="assets/img/team-1.jpg" style="width: 372px;"></div>
-                            <div class="col">
-                                <div class="row">
-                                    <div class="col-xxl-12" style="padding-bottom: 15px;">
-                                        <h2>Doç. Dr. Mustafa Yağcı(Bölüm Başkanı)</h2>
-                                    </div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Fakülte:</strong><span style="margin-left: 5px;">Mimarlık-Mühendislik Fakültesi</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Bölüm:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Ofis:</strong><span style="margin-left: 5px;">--------------------</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Telefon:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>E-posta:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><i class="fa fa-link fs-6"></i><a class="fs-5" href="https://akademik.ahievran.edu.tr/personel/dce4744b-51fe-4518-b580-ac14b49a524e" style="margin-left: 3px;color: var(--bs-gray-900);" target="_blank">Doç. Dr. Mustafa Yağcı</a></div>
+<main class="page blog-post-list">
+    <section class="clean-block clean-blog-list dark">
+        <div class="container-fluid">
+            <div class="block-heading" style="margin-bottom: 53px;">
+                <h2 class="text-info">Akademik Kadromuz</h2>
+            </div>
+            <div class="block-content">
+                <div class="clean-blog-post">
+                    <div class="row" style="box-shadow: 0px 0px 7px var(--bs-gray-400);">
+                        <div class="col-md-auto col-lg-auto col-xl-auto"><img class="img-fluid" src="assets/img/team-1.jpg" style="width: 372px;"></div>
+                        <div class="col">
+                            <div class="row justify-content-end">
+                                <div class="col-xxl-12" style="padding-bottom: 15px;">
+                                    <h1>Doç. Dr. Mustafa Yağcı(Bölüm Başkanı)<br></h1>
                                 </div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Fakülte:</strong><span style="margin-left: 5px;">Mimarlık-Mühendislik Fakültesi</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Bölüm:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Ofis:</strong><span style="margin-left: 5px;">--------------------</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Telefon:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>E-posta:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><i class="fa fa-link fs-6"></i><a class="fs-5" href="https://akademik.ahievran.edu.tr/personel/dce4744b-51fe-4518-b580-ac14b49a524e" style="margin-left: 3px;color: var(--bs-gray-900);" target="_blank">Doç. Dr. Mustafa Yağcı</a></div>
+                                <div class="col-lg-auto offset-md-11 offset-lg-7 offset-xl-10 offset-xxl-10 text-center"><button class="btn btn-primary" type="button" data-bs-target="#modal-1" data-bs-toggle="modal">Video Mesajı</button></div>
                             </div>
                         </div>
                     </div>
-                    <div class="clean-blog-post">
-                        <div class="row" style="box-shadow: 0px 0px 7px var(--bs-gray-400);">
-                            <div class="col-md-auto col-lg-auto col-xl-auto"><img class="img-fluid" src="assets/img/team-1.jpg" style="width: 372px;"></div>
-                            <div class="col">
-                                <div class="row">
-                                    <div class="col-xxl-12" style="padding-bottom: 15px;">
-                                        <h2>Doç. Dr. Mustafa Yağcı(Bölüm Başkanı)</h2>
-                                    </div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Fakülte:</strong><span style="margin-left: 5px;">Mimarlık-Mühendislik Fakültesi</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Bölüm:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Ofis:</strong><span style="margin-left: 5px;">--------------------</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Telefon:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>E-posta:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
-                                    <div class="col-xxl-12" style="padding-bottom: 7px;"><i class="fa fa-link fs-6"></i><a class="fs-5" href="https://akademik.ahievran.edu.tr/personel/dce4744b-51fe-4518-b580-ac14b49a524e" style="margin-left: 3px;color: var(--bs-gray-900);" target="_blank">Doç. Dr. Mustafa Yağcı</a></div>
+                    <div class="modal fade" role="dialog" tabindex="-1" id="modal-1">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h6 class="modal-title">Video Mesajı</h6><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
+                                <div class="modal-body">
+                                    <div><video width="560" height="315" controls="" style="max-width: 100%;">
+                                            <source type="video/mp4">
+                                        </video></div>
+                                </div>
+                                <div class="modal-footer"><button class="btn btn-light" type="button" data-bs-dismiss="modal">Kapat</button></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="clean-blog-post">
+                    <div class="row" style="box-shadow: 0px 0px 7px var(--bs-gray-400);">
+                        <div class="col-md-auto col-lg-auto col-xl-auto"><img class="img-fluid" src="assets/img/team-1.jpg" style="width: 372px;"></div>
+                        <div class="col">
+                            <div class="row">
+                                <div class="col-xxl-12" style="padding-bottom: 15px;">
+                                    <h2>Doç. Dr. Mustafa Yağcı(Bölüm Başkanı)</h2>
+                                </div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Fakülte:</strong><span style="margin-left: 5px;">Mimarlık-Mühendislik Fakültesi</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Bölüm:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Ofis:</strong><span style="margin-left: 5px;">--------------------</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Telefon:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>E-posta:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                                <div class="col-xxl-12" style="padding-bottom: 7px;"><i class="fa fa-link fs-6"></i><a class="fs-5" href="https://akademik.ahievran.edu.tr/personel/dce4744b-51fe-4518-b580-ac14b49a524e" style="margin-left: 3px;color: var(--bs-gray-900);" target="_blank">Doç. Dr. Mustafa Yağcı</a></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
-    </main>
+            <div class="clean-blog-post">
+                <div class="row" style="box-shadow: 0px 0px 7px var(--bs-gray-400);">
+                    <div class="col-md-auto col-lg-auto col-xl-auto"><img class="img-fluid" src="assets/img/team-1.jpg" style="width: 372px;"></div>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col-xxl-12" style="padding-bottom: 15px;">
+                                <h2>Doç. Dr. Mustafa Yağcı(Bölüm Başkanı)</h2>
+                            </div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Fakülte:</strong><span style="margin-left: 5px;">Mimarlık-Mühendislik Fakültesi</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Bölüm:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Ofis:</strong><span style="margin-left: 5px;">--------------------</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Telefon:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>E-posta:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><i class="fa fa-link fs-6"></i><a class="fs-5" href="https://akademik.ahievran.edu.tr/personel/dce4744b-51fe-4518-b580-ac14b49a524e" style="margin-left: 3px;color: var(--bs-gray-900);" target="_blank">Doç. Dr. Mustafa Yağcı</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="clean-blog-post">
+                <div class="row" style="box-shadow: 0px 0px 7px var(--bs-gray-400);">
+                    <div class="col-md-auto col-lg-auto col-xl-auto"><img class="img-fluid" src="assets/img/team-1.jpg" style="width: 372px;"></div>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col-xxl-12" style="padding-bottom: 15px;">
+                                <h2>Doç. Dr. Mustafa Yağcı(Bölüm Başkanı)</h2>
+                            </div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Fakülte:</strong><span style="margin-left: 5px;">Mimarlık-Mühendislik Fakültesi</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Bölüm:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Ofis:</strong><span style="margin-left: 5px;">--------------------</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>Telefon:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><strong>E-posta:</strong><span style="margin-left: 5px;">Bilgisayar Mühendisliği</span></div>
+                            <div class="col-xxl-12" style="padding-bottom: 7px;"><i class="fa fa-link fs-6"></i><a class="fs-5" href="https://akademik.ahievran.edu.tr/personel/dce4744b-51fe-4518-b580-ac14b49a524e" style="margin-left: 3px;color: var(--bs-gray-900);" target="_blank">Doç. Dr. Mustafa Yağcı</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+</main>
 <?php include 'footer.php'; ?>
