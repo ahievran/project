5922a853acaafef0c2133b13de4652f01e7e444e
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-03-23 00:28:22 +0300

    format

--- admin/hedeflerimiz.php
+++ admin/hedeflerimiz.php
@@ -4,99 +4,104 @@ require_once 'sidebar.php';
 include '../baglanti.php';
 ?>
 <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Hedeflerimiz</h1>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Simple Tables</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Hedeflerimiz</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Simple Tables</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-  <!-- Main content -->
-  <section class="content">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Hedeflerimiz</h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <table class="table table-striped projects">
-                <thead>
-                <tr>
-                  <th>
-                    #
-                  </th>
-                  <th>
-                    İcon
-                  </th>
-                  <th>
-                    Hedeflerimiz Başlık
-                  </th>
-                  <th>
-                    Hedeflerimiz İçerik
-                  </th>
-                  <th>
-                    Sil
-                  </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>1.</td>
-                  <td><i class="icon ion-android-globe" style="font-size: 40px;"></i></td>
-                  <td>Update softwareUpdate softwareUpdate softwareUpdate softwareUpdate software</td>
-                  <td>
-                    <a>
-                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                        Görüntüle
-                      </button>
-                      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h5 class="modal-title" id="exampleModalLabel">İçerik</h5>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div class="modal-body">
-                            Hedeflerimiz İçeriği
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
-                            </div>
-                          </div>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Hedeflerimiz</h3>
                         </div>
-                      </div>
-                    </a>
-                  </td>
-                  <td>
-                    <a>
-                      <button class="btn btn-danger">Sil</button>
-                    </a>
-                  </td>
-                </tr>
-                </tbody>
-              </table>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table class="table table-striped projects">
+                                <thead>
+                                <tr>
+                                    <th>
+                                        #
+                                    </th>
+                                    <th>
+                                        İcon
+                                    </th>
+                                    <th>
+                                        Hedeflerimiz Başlık
+                                    </th>
+                                    <th>
+                                        Hedeflerimiz İçerik
+                                    </th>
+                                    <th>
+                                        Sil
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>1.</td>
+                                    <td><i class="icon ion-android-globe" style="font-size: 40px;"></i></td>
+                                    <td>Update softwareUpdate softwareUpdate softwareUpdate softwareUpdate software</td>
+                                    <td>
+                                        <a>
+                                            <button type="button" class="btn btn-primary" data-toggle="modal"
+                                                    data-target="#exampleModal">
+                                                Görüntüle
+                                            </button>
+                                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                                                 aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">İçerik</h5>
+                                                            <button type="button" class="close" data-dismiss="modal"
+                                                                    aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Hedeflerimiz İçeriği
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary"
+                                                                    data-dismiss="modal">Kapat
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a>
+                                            <button class="btn btn-danger">Sil</button>
+                                        </a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
-  <!-- /.content -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
 </div>
 <?php
 require_once 'footer.php'; ?>
