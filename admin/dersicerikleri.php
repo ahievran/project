342a0044d41b12c5df86ab080bd0a64f1d76c9fe
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-03-23 01:16:48 +0300

    ders içerikleri linkini databaseye yüklemek için bir takım uğraşlar

--- admin/dersicerikleri.php
+++ admin/dersicerikleri.php
@@ -4,49 +4,62 @@ require_once 'sidebar.php';
 include '../baglanti.php';
 ?>
 <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Ders İçerikleri Link</h1>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Ders İçerikleri Link</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
-
-  <!-- Main content -->
-  <section class="content">
-    <div class="container-fluid">
-      <div class="row justify-content-md-center">
-        <div class="col-md-8">
-        <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Ders İçerikleri Linki</h3>
-              </div>
-              <form>
-                <div class="card-body">
-                  <div class="form-group">
-                    <label for="link">Link</label>
-                    <input type="text" class="form-control" id="link" placeholder="Link Giriniz">
-                  </div>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Ders İçerikleri Link</h1>
                 </div>
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Gönder</button>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Ders İçerikleri Link</li>
+                    </ol>
                 </div>
-              </form>
             </div>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
-  <!-- /.content -->
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <?php
+    if (isset($_POST["gonder"])){
+        $link = $_POST["link"];
+    }
+
+//    $sql = "INSERT INTO ders_icerik (ders_icerik_url) VALUE ($link)";
+//    $result = $db->query($sql);
+//    while($row = $result->fetch_array()){
+
+    ?>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row justify-content-md-center">
+                <div class="col-md-8">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Ders İçerikleri Linki</h3>
+                        </div>
+                        <form method="POST">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="link">Link</label>
+                                    <input type="text" class="form-control" id="link" name="link" placeholder="Link Giriniz">
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary" id="gonder" name="gonder">Gönder</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <?php
+//}
+    ?>
+    <!-- /.content -->
 </div>
 <?php
 require_once 'footer.php'; ?>
