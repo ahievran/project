13f4c6b12de96725ba5f3b154aec14fd323dcbb8
Cemrdeniz <100775818+Cemrdeniz@users.noreply.github.com>
2022-03-25 13:56:33 +0300

    Add files via upload

--- iletisimform.php
+++ iletisimform.php
@@ -1,18 +1,18 @@
 
-<?php error_reporting(0);
+<?php 
 session_start();
 require_once 'baglanti.php';
 
 $konu 	=(trim($_POST['subject']));
 $ad		=(trim($_POST['name']));
 $mail	=(trim($_POST['email']));
-$mesaj	=(trim($_POST['message']));
-$ip = $_SERVER['REMOTE_ADDR'];
+$message	=(trim($_POST['message']));
+
 if(isset($_POST['send'])){
     if(filter_var($mail,FILTER_VALIDATE_EMAIL)){
-    if($konu !="" and $ad !="" and $mail !="" and $mesaj!=""){    
-        $dkayit=$kayit="INSERT INTO `mesaj` (`mesaj_id`, `mesaj_isim_soyisim`, `mesaj_konu`, `mesaj_email`, `mesaj_icerik`) VALUES ('$ip', '$ad', '$konu', '$mail', '$message');";
-        $dsonuc=mysqli_query($baglanti,$dkayit);  
+    if($konu !="" and $ad !="" and $mail !="" and $message!=""){    
+        $dkayit=$dkayit="INSERT INTO `mesaj` (`mesaj_isim_soyisim`, `mesaj_konu`, `mesaj_email`, `mesaj_icerik`) VALUES ('$ad', '$konu', '$mail', '$message')";
+        $dsonuc=mysqli_query($db,$dkayit);  
         if (isset ($dkayit)){
             echo '<script type="text/javascript">alert("MESAJINIZ GÖNDERİLDİ");</script>';
             echo '<meta http-equiv="refresh" content="0;URL=iletisim.php">';    
