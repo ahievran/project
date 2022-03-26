008be74fe439020ffac2bdb5c76b16081b394277
sametadsan <100577667+sametadsan@users.noreply.github.com>
2022-03-26 13:04:35 +0300

    web galeri

--- galeri.php
+++ galeri.php
@@ -6,14 +6,12 @@
                     <h2 class="text-info">Galeri</h2>
                 </div>
                 <div class="row">
-                    <div class="col-md-6 col-lg-4 item" style="height: 281px;max-height: 281px;"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/gallery-1.jpg" style="height: 281px;width: 416px;"></a></div>
-                    <div class="col-md-6 col-lg-4 item" style="height: 281px;max-height: 281px;"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/gallery-1.jpg" style="height: 281px;width: 416px;"></a></div>
-                    <div class="col-md-6 col-lg-4 item" style="height: 281px;max-height: 281px;"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/gallery-1.jpg" style="height: 281px;width: 416px;"></a></div>
-                    <div class="col-md-6 col-lg-4 item" style="height: 281px;max-height: 281px;"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/gallery-1.jpg" style="height: 281px;width: 416px;"></a></div>
-                    <div class="col-md-6 col-lg-4 item" style="height: 281px;max-height: 281px;"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/gallery-1.jpg" style="height: 281px;width: 416px;"></a></div>
-                    <div class="col-md-6 col-lg-4 item" style="height: 281px;max-height: 281px;"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/gallery-1.jpg" style="height: 281px;width: 416px;"></a></div>
-                    <div class="col-md-6 col-lg-4 item" style="height: 281px;max-height: 281px;"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/gallery-1.jpg" style="height: 281px;width: 416px;"></a></div>
-                    <div class="col-md-6 col-lg-4 item" style="height: 281px;max-height: 281px;"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/gallery-1.jpg" style="height: 281px;width: 416px;"></a></div>
+                <?php
+                    $sql = "SELECT * FROM galeri ORDER BY galeri_id ASC";
+                    $result = $db->query($sql);
+                    while($row = $result->fetch_array()){ ?>
+                    <div class="col-md-6 col-lg-4 item" style="height: 281px;max-height: 281px;"><a class="lightbox" href="<?php echo $row["galeri_resim"]; ?>"><img class="img-thumbnail img-fluid image" src="<?php echo $row["galeri_resim"]; ?>" style="height: 281px;width: 416px;"></a></div>
+                    <?php  } ?>
                 </div>
             </div>
         </section>
