146e5f4d00296c03dd98c4cdf2e66ea4a63e26c4
sametadsan <100577667+sametadsan@users.noreply.github.com>
2022-03-27 17:20:08 +0300

    lab web kısmı bitti

--- labs.php
+++ labs.php
@@ -6,38 +6,20 @@
                     <h2 class="text-info">Laboratuvarlarımız</h2>
                 </div>
                 <div class="row">
+                <?php
+                    $sql = "SELECT * FROM laboratuvar ORDER BY lab_id ASC";
+                    $result = $db->query($sql);
+                    while($row = $result->fetch_array()){ ?>
                     <div class="col-lg-4">
                         <div class="card clean-testimonial-item border-0 rounded-0">
                             <div class="card-body">
-                                <div><img class="img-fluid" src="assets/img/image-2.jpeg" style="width: 400px;"></div>
-                                <h3 style="text-align: center;margin-top: 8px;margin-bottom: 0px;">Bilgisayar Laboratuvarı</h3>
+                                <div><img class="img-fluid" src="<?php echo $row["lab_resim"];  ?>" style="width: 400px; height:350px;"></div>
+                                <h3 style="text-align: center;margin-top: 28px;margin-bottom: 0px;"><?php echo $row["lab_ad"]; ?></h3>
+                                <p style="text-align: center;margin-top: 15px;margin-bottom: 0px;"><?php echo $row["lab_aciklama"] ?> </p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="card clean-testimonial-item border-0 rounded-0">
-                            <div class="card-body">
-                                <div><img class="img-fluid" src="assets/img/image-2.jpeg" style="width: 400px;"></div>
-                                <h3 style="text-align: center;margin-top: 8px;margin-bottom: 0px;">Bilgisayar Laboratuvarı</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card clean-testimonial-item border-0 rounded-0">
-                            <div class="card-body">
-                                <div><img class="img-fluid" src="assets/img/image-2.jpeg" style="width: 400px;"></div>
-                                <h3 style="text-align: center;margin-top: 8px;margin-bottom: 0px;">Bilgisayar Laboratuvarı</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card clean-testimonial-item border-0 rounded-0">
-                            <div class="card-body">
-                                <div><img class="img-fluid" src="assets/img/image-2.jpeg" style="width: 400px;"></div>
-                                <h3 style="text-align: center;margin-top: 8px;margin-bottom: 0px;">Bilgisayar Laboratuvarı</h3>
-                            </div>
-                        </div>
-                    </div>
+                    <?php } ?>
                 </div>
             </div>
         </section>
