9e90238939941c99dc4c9f0dfd40948a768a1da9
kaynarburak <62210378+kaynarburak@users.noreply.github.com>
2022-04-15 16:05:47 +0300

    Update hakkimizda.php

--- admin/hakkimizda.php
+++ admin/hakkimizda.php
@@ -62,28 +62,9 @@ include '../baglanti.php';
                                             <td><?php echo $row["hakkinda_icerik"]; ?></td>
                                             <td>
                                                 <a>
-                                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                                    <button type="button" class="btn btn-primary">
                                                         Görüntüle
                                                     </button>
-                                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                        <div class="modal-dialog" role="document">
-                                                            <div class="modal-content">
-                                                                <div class="modal-header">
-                                                                    <h5 class="modal-title" id="exampleModalLabel">İçerik</h5>
-                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                        <span aria-hidden="true">&times;</span>
-                                                                    </button>
-                                                                </div>
-                                                                <div class="modal-body">
-                                                                <?php echo $row["hakkinda_icerik"]; ?>
-                                                                </div>
-                                                                <div class="modal-footer">
-                                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat
-                                                                    </button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
                                                 </a>
                                             </td>
                                             <td>
