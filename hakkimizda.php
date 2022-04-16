c5329cae39f9fd185cd80d5626bc90f4c711754d
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-04-16 15:48:18 +0300

    ✨ Hakkımızda kısmı statiklikten tamamen kurtarıldı, tüm veriler databaseden çekilmekte

--- hakkimizda.php
+++ hakkimizda.php
@@ -1,25 +1,26 @@
 <?php include 'header.php'; ?>
+
     <main class="page">
         <section class="clean-block about-us">
             <div class="container-fluid">
                 <div class="block-heading">
                     <h2 class="text-info">Hakkımızda</h2>
                 </div>
+
                 <div class="row">
                     <div class="col-xxl-6"><img src="assets/img/about-img.jpg" style="width: 103%;padding-bottom: 14px;padding-top: 14px;"></div>
                     <div class="col-xxl-6" style="padding-right: 24px;padding-left: 24px;">
+                        <?php
+                        $sql = "SELECT * FROM hakkimizda ORDER BY hakkinda_id ASC";
+                        $result = $db->query($sql);
+                        while ($row = $result->fetch_array()) { ?>
                         <ul class="list-unstyled">
-                            <li style="height: auto;text-align: justify;font-size: 12.4px;padding: 0px;padding-top: 0px;padding-bottom: 0px;"><i class="icon ion-android-checkmark-circle" style="font-size: 20px;color: var(--bs-teal);"></i>&nbsp;2018-2019 eğitim öğretim yılı güz döneminde, Mühendislik Mimarlık Fakültesi bünyesinde öğrenci alımına başlayan bölümümüz; 1 Doçent, 4 doktor öğretim üyesi ve 3 araştırma görevlisi ile eğitim öğretime devam etmektedir.<br></li>
-                            <li style="height: auto;text-align: justify;padding: 0px;padding-bottom: 0px;padding-top: 0px;font-size: 12.4px;"><i class="icon ion-android-checkmark-circle" style="font-size: 20px;color: var(--bs-teal);"></i>&nbsp;Bölümümüze öğrenci kabulü; Yükseköğretim Kurumu (YÖK) tarafından belirlenen yönetmelikler çerçevesinde, merkezi sınav sistemi ile yapılmaktadır. Öğrenciler, öğrenim görmek istedikleri program tercihlerini bildirdikten sonra bu sınavdan aldıkları puana göre ÖSYM tarafından ilgili programlara yerleştirilmektedir.<br></li>
-                            <li style="height: auto;text-align: justify;font-size: 12.4px;padding: 0px;padding-top: 0px;padding-bottom: 0px;"><i class="icon ion-android-checkmark-circle" style="font-size: 20px;color: var(--bs-teal);"></i>&nbsp;Bölümümüz 2 öğrenci okul birincisi kontenjanı olmak üzere toplam 62 öğrenci kontenjanına sahip olmakla birlikte açıldığı eğitim öğretim yılından bu yana %100 ilk yerleştirme oranına sahiptir. İlaveten bölümümüzde 2020-2021 eğitim öğretim yılı itibarıyla 222 öğrencimiz öğrenim görmektedir.<br></li>
-                            <li style="height: auto;text-align: justify;font-size: 12.4px;padding: 0px;padding-top: 0px;padding-bottom: 0px;"><i class="icon ion-android-checkmark-circle" style="font-size: 20px;color: var(--bs-teal);"></i>&nbsp;Bölümümüzü başarı ile tamamlayan öğrenciler Bilgisayar Mühendisliği alanında lisans derecesi almaya hak kazanmaktadırlar.<br></li>
-                            <li style="height: auto;text-align: justify;font-size: 12.4px;padding: 0px;padding-top: 0px;padding-bottom: 0px;"><i class="icon ion-android-checkmark-circle" style="font-size: 20px;color: var(--bs-teal);"></i>&nbsp;Öğrencilerimizin mezun olabilmeleri için asgari 240 AKTS’lik (4 yıl - 159 ulusal kredi) müfredatı, en az 2.00 ağırlıklı genel not ortalaması ile azami 7 yılda, 60 işgünü zorunlu staj ile birlikte tamamlamış olmaları gerekmektedir.<br></li>
-                            <li style="height: auto;text-align: justify;font-size: 12.4px;padding: 0px;padding-top: 0px;padding-bottom: 0px;"><i class="icon ion-android-checkmark-circle" style="font-size: 20px;color: var(--bs-teal);"></i>Öğrencilerimiz eğitim süreleri boyunca alacakları zorunlu derslerin yanında, 47 farklı seçmeli ders içerisinden seçecekleri dersler sayesinde yazılım geliştirme, elektronik, yapay zeka, siber güvenlik gibi birçok alanda uzmanlaşabileceklerdir.<br></li>
-                            <li style="height: auto;text-align: justify;font-size: 12.4px;padding: 0px;padding-top: 0px;padding-bottom: 0px;"><i class="icon ion-android-checkmark-circle" style="font-size: 20px;color: var(--bs-teal);"></i>&nbsp;Programımızdan mezun olacak bilgisayar mühendisleri, gerek kamu kurum ve kuruluşlarında, gerekse hem yurt içi hemde yurt dışında; mobil, web veya masaüstü uygulama geliştirme, sistem yöneticiliği, bilgi güvenliği gibi alanlarda görev alabilecek, ayrıca kendi bilişim firmalarını kurarak ülke ekonomisinin gelişmesinde aktif rol oynayabileceklerdir.<br></li>
-                            <li style="height: auto;text-align: justify;font-size: 12.4px;padding: 0px;padding-top: 0px;padding-bottom: 0px;"><i class="icon ion-android-checkmark-circle" style="font-size: 20px;color: var(--bs-teal);"></i>&nbsp;Bölümümüzden mezun olan öğrenciler, lisansüstü programlarda öğrenim görmek üzere başvuruda bulunabilirler.<br></li>
+                            <li style="position: relative"><i class="icon ion-android-checkmark-circle" style="font-size: 20px;color: var(--bs-teal);"></i> <?php echo $row["hakkinda_icerik"]; ?><br></li>
                         </ul>
+                        <?php } ?>
                     </div>
                 </div>
+
             </div>
         </section>
     </main>
