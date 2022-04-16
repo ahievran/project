e5032c70a59c6602575977298761e705483742e9
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-04-16 15:53:18 +0300

    ✨ Hedeeflerimiz kısmı statiklikten tamamen kurtarıldı, tüm veriler databaseden çekilmekte

--- hedeflerimiz.php
+++ hedeflerimiz.php
@@ -5,25 +5,20 @@
                 <div class="block-heading">
                     <h2 class="text-info">Hedeflerimiz</h2>
                     <div class="row">
-                        <div class="col-md-5 col-lg-4"><img src="assets/img/product-features.png" style="width: 100%;"></div>
+                        <div class="col-md-5 col-lg-4"><img src="assets/img/product-features.png" style="width: 100%;">
+                        </div>
                         <div class="col" style="padding-top: 27px;">
                             <div class="row" style="width:90%; margin-left: 20px;">
-                                <div class="col-md-6 col-lg-6"><i class="icon ion-android-globe" style="font-size: 40px;"></i>
-                                    <h4>Uluslararası Araştırmalar</h4>
-                                    <p style="text-align: center;">Bilgisayar Mühendisliği alanında standartların oluşturulduğu ulusal ve uluslar arası çalışmalara katkı ve destek sağlamak.<br></p>
-                                </div>
-                                <div class="col-md-6 col-lg-6"><i class="icon ion-university" style="font-size: 40px;"></i>
-                                    <h4>Üniversite ve Sanayi İşbirliği<br></h4>
-                                    <p style="text-align: center;">Üniversite ve sanayi işbirliğini en üst seviyelere taşımak, sanayinin ve bilişim sektörünün ihtiyaçlarına göre araştırmalar yaparak projeler geliştirmek<br></p>
-                                </div>
-                                <div class="col-md-6 col-lg-6"><i class="icon ion-briefcase" style="font-size: 40px;"></i>
-                                    <h4>Sektör İhtiyaçlarını Giderme<br></h4>
-                                    <p style="text-align: center;">Gerek kamu kurum ve kuruluşlarında, gerekse yurt içi/ yurt dışı firmalarda; mobil, web veya masaüstü uygulama geliştirme, sistem yöneticiliği, bilgi güvenliği gibi alanlarda görev alabilecek yetenekli bilgisayar mühendisleri yetiştirmek.<br></p>
-                                </div>
-                                <div class="col-md-6 col-lg-6"><i class="icon ion-ios-paper" style="font-size: 40px;"></i>
-                                    <h4>Disiplinler Arası Çalışma<br></h4>
-                                    <p style="text-align: center;">Programlama, yapay zeka, makine öğrenmesi, derin öğrenme, siber güvenlik gibi alanlarda yeterlilik sahibi bilgisayar mühendisleri yetiştirerek, farklı anabilim dallarının katılımı ile gerçekleştirilecek disiplinler arası birçok projede aktif olarak yer almalarını sağlamak.<br></p>
-                                </div>
+                                <?php
+                                $sql = "SELECT * FROM hedeflerimiz ORDER BY hedef_id ASC";
+                                $result = $db->query($sql);
+                                while ($row = $result->fetch_array()) { ?>
+                                    <div class="col-md-6 col-lg-6"><i class="icon ion-android-globe"
+                                                                      style="font-size: 40px;"></i>
+                                        <h4><?php echo $row["hedef_baslik"]; ?></h4>
+                                        <p style="text-align: center;"><?php echo $row["hedef_icerik"]; ?><br></p>
+                                    </div>
+                                <?php } ?>
                             </div>
                         </div>
                     </div>
