17b63dcc5abbde7ee70773149f7fe3ef747ef255
kaynarburak <62210378+kaynarburak@users.noreply.github.com>
2022-04-19 16:11:00 +0300

    event add

---
+++ admin/eventgoruntule.php
@@ -0,0 +1,53 @@
+<?php
+require_once '../admin/header.php';
+require_once 'sidebar.php';
+include '../baglanti.php';
+?>
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Etkinlik Adı</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item active">Etkinlik Adı</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="card card-solid">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-5">
+                        <div class="col-12">
+                            <img src="../assets/img/cerenAkman.jpeg" class="product-image" alt="Laboratuvar Resim">
+                        </div>
+                    </div>
+                    <div class="col-1">
+                        
+                    </div>
+                    <div class="col-6">
+                        <h3 class="my-3">Etkinlik Adı:</h3>
+                        <p>Etkinlik Açıklaması:</p>
+                        <p>Etkinlik Tarihi:</p>
+                </div>
+
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+</div>
+
+<?php require_once 'footer.php'; ?>
--- admin/events.php
+++ admin/events.php
@@ -73,28 +73,10 @@ include '../baglanti.php';
                                                 <?php echo $row["etkinlik_icerik"]; ?>
                                             </td>
                                             <td>
-                                                <a>
+                                                <a href="eventgoruntule.php">
                                                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                                                         Görüntüle
                                                     </button>
-                                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                        <div class="modal-dialog" role="document">
-                                                            <div class="modal-content">
-                                                                <div class="modal-header">
-                                                                    <h5 class="modal-title" id="exampleModalLabel">İçerik</h5>
-                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                        <span aria-hidden="true">&times;</span>
-                                                                    </button>
-                                                                </div>
-                                                                <div class="modal-body">
-                                                                    içerik
-                                                                </div>
-                                                                <div class="modal-footer">
-                                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
                                                 </a>
                                             </td>
                                             <td>
