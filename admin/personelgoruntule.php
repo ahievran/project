97ff8d9f150be29fe268318cd297131ca56d0dcd
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-04-19 21:21:42 +0300

    ✨ Personel görüntüleme tamamlandı

--- admin/personelgoruntule.php
+++ admin/personelgoruntule.php
@@ -4,57 +4,87 @@ require_once 'sidebar.php';
 include '../baglanti.php';
 ?>
 
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Contacts</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Contacts</li>
-                        </ol>
-                    </div>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Contacts</h1>
                 </div>
-            </div><!-- /.container-fluid -->
-        </section>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Contacts</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-        <!-- Main content -->
-        <section class="content">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-12 w-100">
-                        <div class="card bg-light d-flex flex-fill">
-                            <div class="card-header text-muted border-bottom-0">
-                                Digital Strategist
-                            </div>
-                            <div class="card-body pt-0">
-                                <div class="row">
-                                    <div class="col-8">
-                                        <h2 class="lead"><b>Nicole Pearson</b></h2>
+    <!-- Main content -->
+    <section class="content">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-12 w-100">
+                    <div class="card bg-light d-flex flex-fill">
+                        <div class="card-header text-muted border-bottom-0">
+                            Digital Strategist
+                        </div>
+                        <div class="card-body pt-0">
+                            <div class="row">
+                                <div class="col-8">
+                                    <?php if (isset($_POST["see"])) {
+                                        $personel_id = $_POST["personel_id"];
+                                        $personel_ad = $_POST["personel_ad"];
+                                        $personel_fakulte = $_POST["personel_fakulte"];
+                                        $personel_bolum = $_POST["personel_bolum"];
+                                        $personel_ofis = $_POST["personel_ofis"];
+                                        $personel_telefon = $_POST["personel_telefon"];
+                                        $personel_email = $_POST["personel_email"];
+                                        $personel_hakkinda = $_POST["personel_hakkinda"];
+                                        $personel_img = $_POST["personel_img"];
+                                        ?>
+                                        <h2 class="lead"><b><?php echo $personel_ad ?></b></h2>
                                         <ul class="ml-4 fa-ul text-muted">
-                                            <li class="large"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Fakülte:</li>
-                                            <li class="large"><span class="fa-li"><i class="fas fa-lg fa-book"></i></span> Bölüm:</li>
-                                            <li class="large"><span class="fa-li"><i class="fas fa-lg fa-door-closed"></i></span> Ofis:</li>
-                                            <li class="large"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Telefon:</li>
-                                            <li class="large"><span class="fa-li"><i class="fas fa-lg fa-envelope"></i></span> E-Mail:</li>
-                                            <li class="large"><span class="fa-li"><i class="fas fa-lg fa-info-circle"></i></span> Hakkında:</li>
+                                            <li class="large"><span class="fa-li"><i
+                                                            class="fas fa-lg fa-building"></i></span>
+                                                Fakülte: <?php echo $personel_fakulte ?>
+                                            </li>
+                                            <li class="large"><span class="fa-li"><i
+                                                            class="fas fa-lg fa-book"></i></span>
+                                                Bölüm: <?php echo $personel_bolum ?>
+                                            </li>
+                                            <li class="large"><span class="fa-li"><i
+                                                            class="fas fa-lg fa-door-closed"></i></span>
+                                                Ofis: <?php echo $personel_ofis ?>
+                                            </li>
+                                            <li class="large"><span class="fa-li"><i
+                                                            class="fas fa-lg fa-phone"></i></span>
+                                                Telefon: <?php echo $personel_telefon ?>
+                                            </li>
+                                            <li class="large"><span class="fa-li"><i
+                                                            class="fas fa-lg fa-envelope"></i></span>
+                                                E-Mail: <?php echo $personel_email ?>
+                                            </li>
+                                            <li class="large"><span class="fa-li"><i
+                                                            class="fas fa-lg fa-info-circle"></i></span>
+                                                Hakkında: <?php echo $personel_hakkinda ?>
+                                            </li>
                                         </ul>
-                                    </div>
-                                    <div class="col-4 text-right">
-                                        <img src="../../dist/img/user1-128x128.jpg" alt="user-avatar" class="img-fluid">
-                                    </div>
+                                    <?php } ?>
+                                </div>
+                                <div class="col-4 text-right">
+                                    <img src="<?php echo $personel_img ?>" alt="user-avatar" class="img-fluid">
                                 </div>
                             </div>
-
                         </div>
+
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
+</div>
 <?php
 require_once 'footer.php'; ?>
