94ee58992539c7605aa33429945fb0d9290c8ba3
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-04-22 11:43:26 +0300

    ✨ New database location

--- baglanti.php
+++ baglanti.php
@@ -1,5 +1,5 @@
 <?php
 
-$db = new mysqli("localhost","root","","bm");
+$db = new mysqli("bdevxikdmuguf3i0fs2i-mysql.services.clever-cloud.com","uwdsmvi41fkhss0g","CVYaPZX7J8gCIvVlzBNE","bdevxikdmuguf3i0fs2i", port: 3306);
 
 ?>
