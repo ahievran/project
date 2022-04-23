29fd1b6151b2bfe90ae72cdb3c61cafc9ee3bc0b
sametadsan <100577667+sametadsan@users.noreply.github.com>
2022-04-23 16:40:55 +0300

    etkinlikler tasarımı tamamen değişti

--- events.php
+++ events.php
@@ -9,22 +9,44 @@ include 'header.php'; ?>
                     <h2 class="text-info">Etkinlikler</h2>
                 </div>
             </div>
-            <section class="article-list">
-                <div class="container">
-                    <div class="intro"></div>
-                    <div class="row articles">
-                        <?php
-                        $sql = "SELECT * FROM etkinlikler ORDER BY etkinlik_id ASC";
+            <section class="blog-posts">
+      <div class="container-fluid">
+        <div class="row">
+        <div class="col-lg-1"></div>
+          <div class="col-lg-10">
+            <div class="all-blog-posts">
+              <div class="row">
+              <?php
+                        $sql = "SELECT * FROM etkinlikler ORDER BY etkinlik_id DESC";
                         $result = $db->query($sql);
-                        while ($row = $result->fetch_array()) { ?>
-                            <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/cerenAkman.jpeg" style="height: 315px;width: 415px;"></a>
-                                <h3 class="name"><?php echo $row["etkinlik_baslik"]; ?><br></h3>
-                                <p class="description"><?php echo $row["etkinlik_icerik"]; ?><br></p><button class="btn btn-primary" type="button" style="margin-top: 21px;color: var(--bs-gray-900);background: rgba(13,110,253,0);border-color: var(--bs-gray-500);">Devamını Oku</button>
-                            </div>
-                        <?php } ?>
+                        while($row = $result->fetch_array()){ ?>
+                <div class="col-lg-6">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="assets/img/bm-1.jpg" alt="">
                     </div>
+                    <div class="down-content">
+                      <span>Kırşehir Ahi Evran Üniversitesi</span>
+                      <a href="#"><h4><?php echo $row["etkinlik_baslik"]; ?></h4></a>
+                      <ul class="post-info">
+                        <li><a href="#"></a>MMF - Bilgisayar Mühendisliği </li>
+                        <li><a href="#"><?php echo $row["etkinlik_tarih"]; ?></a></li>
+                      </ul>
+                      <p><?php echo $row["etkinlik_icerik"]; ?></p>
+                      <div class="post-options">
+                       <div class='row'>
+                       <div class="col-lg-12">
+                  <div class="main-button" id='devami'>
+                    <a class='text-dark' href="#">Devamını Oku</a>
+                  </div>
                 </div>
-            </section>
+                       </div>
+                      </div>
+                    </div>
+                  </div>
+                </div> <?php } ?>
+               
+                <div class="col-lg-1"></div>
         </section>
     </main>
 <?php include 'footer.php'; ?>
--- header.php
+++ header.php
@@ -21,6 +21,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/anasayfaslider.css">
     <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/etkinlik.css">
 
 </head>
 
