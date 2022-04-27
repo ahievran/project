3b92dcbc7a30ab2bbd81b2c324831293571b339d
kaynarburak <62210378+kaynarburak@users.noreply.github.com>
2022-04-27 16:28:28 +0300

    loginpage

---
+++ admin/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Ahi Evran Üniversitesi | Bilgisayar Mühendisliği</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <link href="https://mf.ahievran.edu.tr/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+    <link rel="stylesheet" href="../assets/fonts/ionicons.min.css">
+
+</head>
+
+<body>
+    <section class="login-dark">
+        <form method="post" action="">
+            <div class="logo">
+                <img src="../assets/img/ahievran_logo.png" alt="Ahi Evran Üniversitesi">
+            </div>
+            <h2>
+                Ahi Evran Üniversitesi
+            </h2>
+            <h3>
+                Bilgisayar Mühendisliği
+            </h3>
+
+            <div class="mb-3"><input class="form-control" type="email" name="kullanici_mail" placeholder="Email"></div>
+            <div class="mb-3"><input class="form-control" type="password" name="kullanici_sifre" placeholder="Şifre"></div>
+            <div class="mb-3"><button class="btn btn-primary d-block w-100" name="adminlogin" type="submit">Giriş Yap</button></div>
+        </form>
+    </section>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
