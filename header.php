08bcd710925e4384aae5fcdfa1b6edcfb599bbe0
sametadsan <100577667+sametadsan@users.noreply.github.com>
2022-04-27 15:01:37 +0300

    footer yapıldı

--- header.php
+++ header.php
@@ -23,6 +23,12 @@
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/etkinlik.css">
     <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link href="https://mf.ahievran.edu.tr/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+
+
 
 </head>
 
