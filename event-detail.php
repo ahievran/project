c1c36288682cbd71f58bab6841249d00cd93a0a0
Burak Kaynar <62210378+kaynarburak@users.noreply.github.com>
2022-05-17 23:06:05 +0300

    Update event-detail.php

--- event-detail.php
+++ event-detail.php
@@ -4,57 +4,73 @@ $_SESSION["sayfa"] = "etkinlikler";
 $_SESSION["title"] = "Etkinlikler";
 $_SESSION["description"] = "Kırşehir Ahi Evran Üniversitesi - Bilgisayar Mühendisliği Etkinlikler";
 include 'header.php'; ?>
-<main class="page blog-post-list">
-    <section class="clean-block clean-blog-list">
-        <section class="clean-block about-us">
-            <div class="heading-page header-text">
-                <section class="page-heading">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="text-content">
-                                    <h4>Kırşehir Ahi Evran Üniversitesi - Bilgisayar Mühendisliği</h4>
-                                    <h2>Etkinlikler</h2>
+    <main class="page blog-post-list">
+        <section class="clean-block clean-blog-list">
+            <section class="clean-block about-us">
+                <div class="heading-page header-text">
+                    <section class="page-heading">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="text-content">
+                                        <h4>Kırşehir Ahi Evran Üniversitesi - Bilgisayar Mühendisliği</h4>
+                                        <h2>Etkinlikler</h2>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </section>
-                <div class="container-fluid">
-                </div>
-                <section class="blog-posts">
+                    </section>
                     <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-lg-1"></div>
-                            <div class="col-lg-10">
-                                <div class="all-blog-posts">
-                                    <div class="row">
-                                        <div class="col-lg-12 animate__animated animate__zoomIn">
-                                            <div class="blog-post">
-                                                <div class="event-thumb">
-                                                    <img src="admin/uploads/events/cerenAkman.jpeg">
+                    </div>
+                    <section class="blog-posts">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-lg-1"></div>
+                                <div class="col-lg-10">
+                                    <div class="all-blog-posts">
+                                        <div class="row">
+                                            <?php if (isset($_POST["see"])) {
+                                                $etkinlik_id = $_POST["etkinlik_id"];
+                                                $etkinlik_baslik = $_POST["etkinlik_baslik"];
+                                                $etkinlik_icerik = $_POST["etkinlik_icerik"];
+                                                $etkinlik_text = $_POST["etkinlik_text"];
+                                                $etkinlik_resim = $_POST["etkinlik_resim"];
+                                                $etkinlik_link = $_POST["etkinlik_link"];
+                                                $etkinlik_tarih = $_POST["etkinlik_tarih"];
+                                                ?>
+                                                <div class="col-lg-12 animate__animated animate__zoomIn">
+                                                    <div class="blog-post">
+                                                        <div class="event-thumb">
+                                                            <a href="<?php echo $etkinlik_link ?>">
+                                                                <img src="admin/<?php echo $etkinlik_resim ?>"
+                                                                     alt="Etkinlik Resim">
+                                                            </a>
+                                                        </div>
+                                                        <div class="down-content">
+                                                            <span>Kırşehir Ahi Evran Üniversitesi</span>
+                                                            <a href="#">
+                                                                <h4><?php echo $etkinlik_baslik ?></h4>
+                                                            </a>
+                                                            <ul class="post-info">
+                                                                <li>MMF - Bilgisayar Mühendisliği</li>
+                                                                <li><?php echo $etkinlik_tarih ?></li>
+                                                            </ul>
+                                                            <p><?php echo $etkinlik_icerik ?></p>
+                                                            <div class="event-text">
+                                                                <?php echo $etkinlik_text ?>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                <div class="down-content">
-                                                    <span>Kırşehir Ahi Evran Üniversitesi</span>
-                                                    <a href="#">
-                                                        <h4>Etkinlik Başlık</h4>
-                                                    </a>
-                                                    <ul class="post-info">
-                                                        <li>MMF - Bilgisayar Mühendisliği </li>
-                                                        <li>Etkinlik Tarihi</li>
-                                                    </ul>
-                                                    <p>Etkinlik İçerik</p>
-                                                </div>
-                                            </div>
+                                            <?php } ?>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </section>
-            </div>
+                    </section>
+                </div>
+            </section>
         </section>
-    </section>
-</main>
+    </main>
 <?php include 'footer.php'; ?>
