427511c5f4b0d6a0e60f7ca66afc06e4b9bf7274
Burak Kaynar <62210378+kaynarburak@users.noreply.github.com>
2022-05-18 13:40:12 +0300

    Create hataligiris.php

---
+++ admin/hataligiris.php
@@ -0,0 +1,68 @@
+<?php
+require_once '../admin/header.php';
+require_once 'sidebar.php';
+include '../baglanti.php';
+?>
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Hatalı Girişler</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Simple Tables</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Hatalı Girişler</h3>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-striped projects">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            #
+                                        </th>
+                                        <th>
+                                            IP Adresi
+                                        </th>
+                                        <th>
+                                            Tarih/Saat
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            1
+                                        </td>
+                                        <td>
+                                        <?php echo $_SERVER ["REMOTE_ADDR"];?>
+                                        </td>
+                                        </td>
+                                        <td>
+                                            18.05.2022 / 13:35
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<?php
+require_once 'footer.php'; ?>
