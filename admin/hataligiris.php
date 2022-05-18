4e13f523c8732c18ec972d8c048b2ae724ecfe35
Burak Kaynar <62210378+kaynarburak@users.noreply.github.com>
2022-05-18 18:30:30 +0300

    Update hataligiris.php

--- admin/hataligiris.php
+++ admin/hataligiris.php
@@ -8,7 +8,7 @@ include '../baglanti.php';
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Hatalı Girişler</h1>
+                    <h1>Hatalı Girişler/Engellenen IP'ler</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -27,25 +27,15 @@ include '../baglanti.php';
                         <div class="card-header">
                             <h3 class="card-title">Hatalı Girişler</h3>
                         </div>
-                        <div class="card-body">
-                            <table class="table table-striped projects">
+                        <div class="card-body table-responsive p-0" style="height: 240px;">
+                            <table class="table table-head-fixed text-nowrap">
                                 <thead>
                                     <tr>
-                                        <th>
-                                            #
-                                        </th>
-                                        <th>
-                                            IP Adresi
-                                        </th>
-                                        <th>
-                                            Tarih/Saat
-                                        </th>
-                                        <th>
-                                            Girilen Kullanıcı Adı
-                                        </th>
-                                        <th>
-                                            Girilen Şifre
-                                        </th>
+                                        <th>ID</th>
+                                        <th>IP Adresi</th>
+                                        <th>Tarih/Saat</th>
+                                        <th>Girilen Kullanıcı Adı</th>
+                                        <th>Girilen Şifre</th>
                                     </tr>
                                 </thead>
                                 <?php
@@ -64,17 +54,61 @@ include '../baglanti.php';
                                                 <?php echo $row['hatali_giris_tarih']; ?>
                                             </td>
                                             <td>
-                                                <?php
-
-
-                                                ?>
+                                                <?php echo $row['admin_kadi']; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row['admin_sifre']; ?>
                                             </td>
                                         </tr>
                                     </tbody>
                                 <?php } ?>
-
                             </table>
                         </div>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Engellenen IP'ler</h3>
+                        </div>
+                        <div class="card-body table-responsive p-0" style="height: 240px;">
+                            <table class="table table-head-fixed text-nowrap">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>IP Adresi</th>
+                                    </tr>
+                                </thead>
+                                <?php
+                                $sql = "SELECT * FROM hatali_giris ORDER BY hatali_giris_id ASC";
+                                $result = $db->query($sql);
+                                while ($row = $result->fetch_array()) { ?>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <?php echo $row['hatali_giris_id']; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row['hatali_giris_ip']; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row['hatali_giris_tarih']; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row['admin_kadi']; ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $row['admin_sifre']; ?>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                <?php } ?>
+                            </table>
+                        </div>
+                        </table>
                     </div>
                 </div>
             </div>
