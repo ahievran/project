d168b5bf22b249b0f4207fc37e7bb6ee834e3013
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-06-12 02:35:04 +0300

    🐛 Haritadaki adres güncellendi

--- admin/iletisim.php
+++ admin/iletisim.php
@@ -8,7 +8,7 @@ include '../baglanti.php';
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Hedeflerimiz</h1>
+                        <h1>İletişim</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
