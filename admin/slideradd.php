3c4c7af9443fd64c378c1a7e05aa4aec5f6c0c2b
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-06-13 13:09:04 +0300

    🐛 wrong image location

--- admin/slideradd.php
+++ admin/slideradd.php
@@ -41,7 +41,7 @@ include '../baglanti.php';
         if (isset($_POST["submit"]) && !empty($_FILES["file"]["name"])) {
             $statusMsg = '';
 
-            $targetDir = "../assets/img";
+            $targetDir = "../assets/img/";
             $fileName = basename($_FILES["file"]["name"]);
             $targetFilePath = $targetDir . $fileName;
             $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
