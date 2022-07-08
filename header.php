22efb3a00adc35fd9fe972a02df380085f494c06
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-07-08 13:07:59 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,114 +1,89 @@
 <?php
+require_once 'connection.php';
 ob_start();
 session_start();
+
+
+$kullanicisor = $db1->prepare("SELECT * FROM admin_giris WHERE admin_kadi=:kadi");
+$kullanicisor->execute(array(
+    'kadi' => $_SESSION['admin_kadi']
+));
+$say = $kullanicisor->rowCount();
+$kullanicicek = $kullanicisor->fetch(PDO::FETCH_ASSOC);
+
+
+if ($say == 0) {
+    header("Location:login.php");
+}
 ?>
 
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title><?php echo $_SESSION["title"]; ?></title>
-    <meta name="description" content="<?php echo $_SESSION["description"]; ?>">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
-    <link rel="stylesheet" href="assets/css/Article-List.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
-    <link rel="stylesheet" href="assets/css/Map-Clean.css">
-    <link rel="stylesheet" href="assets/css/untitled.css">
-    <link rel="stylesheet" href="assets/css/vanilla-zoom.min.css">
-    <link rel="stylesheet" href="assets/css/header.css" />
-    <link rel="stylesheet" href="assets/css/anasayfa.css" />
-    <link rel="stylesheet" href="assets/css/akademik.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/anasayfaslider.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-    <link rel="stylesheet" href="assets/css/etkinlik.css">
-    <link rel="stylesheet" href="assets/css/sss.css">
-    <link rel="stylesheet" href="assets/css/anasayfacarousel.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://mf.ahievran.edu.tr/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
-            integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
-    <script src="assets/js/navbar.js"></script>
-   
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AEU-BM Admin Panel</title>
 
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/60d240fdbb.js" crossorigin="anonymous"></script>
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link href="https://mf.ahievran.edu.tr/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+    <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+
+    <script src="//cdn.ckeditor.com/4.18.0/standard/ckeditor.js"></script>
+    
 
 </head>
 
-<body>
-<div id="preloader">
-    <div class="jumper">
-        <div>
-            <img src='assets/img/pnglogo.png' id='yuklenmelogo'></img>
-        </div>
-    </div>
-</div>
-<?php include 'baglanti.php'; ?>
-<nav class="autohide navbar navbar-light navbar-expand-lg fixed-top clean-navbar" id="navbar">
-    <div class="container-fluid">
-        <img class='animate__animated animate__fadeInBottomRight' src="assets/img/pnglogo.png" id="logo">
-        <div class="col-sm-3">
-            <h1 class='animate__animated animate__fadeInBottomRight' id="logo-sag-yazi1">Mühendislik Mimarlık Fakültesi</h1>
-            <h2 class='animate__animated animate__fadeInBottomRight' id="logo-sag-yazi2">Bilgisayar Mühendisliği</h2>
-        </div>
-        <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navcol-1" style="height: -21.8px;">
-            <ul class="navbar-nav ms-auto" id="ul">
-                <li class="nav-item <?php if ($_SESSION["sayfa"] == 'index') {
-                    echo 'active';
-                } ?>"><a class="nav-link <?php if ($_SESSION["sayfa"] == 'index') {
-                        echo 'active';
-                    } ?>" href="index.php">Ana Sayfa</a></li>
-                <li class="nav-item <?php if ($_SESSION["sayfa"] == 'hakkimizda') {
-                    echo 'active';
-                } ?>"><a class="nav-link <?php if ($_SESSION["sayfa"] == 'hakkimizda') {
-                        echo 'active';
-                    } ?>" href="hakkimizda.php">Hakkımızda</a></li>
-                <li class="nav-item <?php if ($_SESSION["sayfa"] == 'hedeflerimiz') {
-                    echo 'active';
-                } ?>"><a class="nav-link <?php if ($_SESSION["sayfa"] == 'hedeflerimiz') {
-                        echo 'active';
-                    } ?>" href="hedeflerimiz.php">hedeflerimiz</a></li>
-                <li class="nav-item <?php if ($_SESSION["sayfa"] == 'what') {
-                    echo 'active';
-                } ?>"><a class="nav-link <?php if ($_SESSION["sayfa"] == 'what') {
-                        echo 'active';
-                    } ?>" href="https://drive.google.com/file/d/1Yb06Gk6EkdBtBqWi571LP36LchOooLb_/view" target="_blank">Ders İçerikleri</a></li>
-                <li class="nav-item <?php if ($_SESSION["sayfa"] == 'labs') {
-                    echo 'active';
-                } ?>"><a class="nav-link <?php if ($_SESSION["sayfa"] == 'labs') {
-                        echo 'active';
-                    } ?>" href="labs.php">Laboratuvarlarımız</a></li>
-                <li class="nav-item <?php if ($_SESSION["sayfa"] == 'akademik') {
-                    echo 'active';
-                } ?>"><a class="nav-link <?php if ($_SESSION["sayfa"] == 'akademik') {
-                        echo 'active';
-                    } ?>" href="akademik.php">Akademik Personel</a></li>
-                <li class="nav-item <?php if ($_SESSION["sayfa"] == 'etkinlikler') {
-                    echo 'active';
-                } ?>"><a class="nav-link <?php if ($_SESSION["sayfa"] == 'etkinlikler') {
-                        echo 'active';
-                    } ?>" href="events.php">Etkinlikler</a></li>
-                <li class="nav-item <?php if ($_SESSION["sayfa"] == 'galeri') {
-                    echo 'active';
-                } ?>"><a class="nav-link <?php if ($_SESSION["sayfa"] == 'galeri') {
-                        echo 'active';
-                    } ?>" href="galeri.php">Galeri</a></li>
-                <li class="nav-item <?php if ($_SESSION["sayfa"] == 'iletisim') {
-                    echo 'active';
-                } ?>"><a class="nav-link <?php if ($_SESSION["sayfa"] == 'iletisim') {
-                        echo 'active';
-                    } ?>" href="iletisim.php">İletişim</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed">
+<div class="wrapper">
+    <nav class="main-header navbar navbar-expand navbar-dark ">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="index.php" class="nav-link">AEU-MMF Computer Engineering Admin Control</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                    <i class="fas fa-search"></i>
+                </a>
+                <div class="navbar-search-block">
+                    <form class="form-inline">
+                        <div class="input-group input-group-sm">
+                            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                            <div class="input-group-append">
+                                <button class="btn btn-navbar" type="submit">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </nav>
